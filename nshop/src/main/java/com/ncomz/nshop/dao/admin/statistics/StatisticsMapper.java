118795086266d4464e85a5049741a6ff61da62c6
ncomz2020 <developer@ncomz.com>
2020-03-16 14:36:46 +0900

    commit 메세지

--- nshop/src/main/java/com/ncomz/nshop/dao/admin/statistics/StatisticsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ncomz.nshop.dao.admin.statistics;
-
-import org.springframework.stereotype.Component;
-
-import com.ncomz.nshop.domain.admin.mypage.MyPage;
-
-@Component
-public interface StatisticsMapper {
-}
--- nshop/src/main/java/com/ncomz/nshop/domain/admin/statistics/Statistics.java
+++
@@ -1,7 +0,0 @@
-package com.ncomz.nshop.domain.admin.statistics;
-
-import com.ncomz.nshop.domain.common.CommonCondition;
-
-public class Statistics extends CommonCondition {
-	
-}
